7f8fb7c6820f34b9fe08638ddd32625f14c6128a
kasereka-michael <kaserekamichael23526@gmail.com>
2024-07-06 13:30:45 +0200

    service in sliding mood

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
     
-   
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1    
+        },
+        765:{
+            slidesPerView: 2   
+        },
+        1024:{
+            slidesPerView: 4   
+        }
+    }
+  });
+  
     
     window.onscroll = function() {
         if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
@@ -19,3 +48,5 @@ $(document).ready(function(){
         behavior: "smooth"
     });
 });
+
+  
